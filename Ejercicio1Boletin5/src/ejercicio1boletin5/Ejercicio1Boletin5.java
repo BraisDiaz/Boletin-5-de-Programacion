b2f20cd1cf3ae9eb434cf98fdb88b07b14c0e145
Brais <Brais@Brais-PC>
2016-10-21 11:43:28 +0200

    Primer commit del ejercicio 1 (Programa para comprobar positividade)

---
+++ Ejercicio1Boletin5/src/ejercicio1boletin5/Ejercicio1Boletin5.java
@@ -0,0 +1,26 @@
+
+package ejercicio1boletin5;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Brais
+ */
+public class Ejercicio1Boletin5 {
+
+    
+
+ 
+    public static void main(String[] args) {
+        int num1;
+        
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Introducir número"));
+        
+        if (num1>0){
+        
+            System.out.println("O número é positivo");
+            
+    }
+    }
+}
